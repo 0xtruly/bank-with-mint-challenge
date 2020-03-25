f26a8da51e224037e5e6158d7172d991ec8a47cf
segun-ade <adebanjosegun589@gmail.com>
2020-03-26 00:57:54 +0100

    :rocket:add utility functions

---
+++ src/components/Dashboard/utils.js
@@ -0,0 +1,35 @@
+export const getRandomDateArray = numItems => {
+    // Create random array of objects (with date)
+    let data = [];
+    let baseTime = new Date().getTime();
+    console.log(baseTime);
+    let dayMs = 24 * 60 * 60 * 1000;
+    for (var i = 0; i < numItems; i++) {
+        data.push({
+            time: new Date(baseTime + i * dayMs),
+            value: Math.round(20 + 80 * Math.random())
+        });
+    }
+    return data;
+};
+
+export const getData = () => {
+    let data = [];
+    data.push({
+        title: 'Daily Transaction Volume',
+        data: getRandomDateArray(3.9)
+    });
+    data.push({
+        title: 'Daily Transaction Value',
+        data: getRandomDateArray(5)
+    });
+    data.push({
+        title: 'Total Transaction Volume',
+        data: getRandomDateArray(5)
+    });
+    data.push({
+        title: 'Total Transaction Value',
+        data: getRandomDateArray(5)
+    });
+    return data;
+};
