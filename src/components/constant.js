bccb22bed47242709aed6af20b72890108fe02c1
segun-ade <adebanjosegun589@gmail.com>
2020-03-25 13:08:51 +0100

    :rocket:add constant file for string characters

---
+++ src/components/constant.js
@@ -0,0 +1,65 @@
+export const LOGO_TEXT = 'TransMonitor';
+
+export const DASHBOARD_MENU_STRING_ONE = [
+    {
+        iconName: 'Overview',
+        key: '1',
+        link: '',
+        text: 'Overview',
+    }
+];
+export const DASHBOARD_MENU_STRING_TWO = [
+    {
+        iconName: 'AllPayments',
+        key: '2',
+        link: '',
+        text: 'All payments',
+    },
+    {
+        iconName: 'ReconcilledPay',
+        key: '3',
+        link: '',
+        text: 'Reconcilled Payments',
+    },
+    {
+        iconName: 'UnreconcilledPay',
+        key: '4',
+        link: '',
+        text: 'Un - reconcilled Paymenents',
+    },
+    {
+        iconName: 'Manual',
+        key: '5',
+        link: '',
+        text: 'Manual Settlement',
+    },
+];
+export const DASHBOARD_MENU_STRING_THREE = [
+    {
+        iconName: 'AllOrders',
+        key: '6',
+        link: '',
+        text: 'AllOrders',
+    },
+    {
+        iconName: 'Pending',
+        key: '7',
+        link: '',
+        text: 'Pending Orders',
+    },
+    {
+        iconName: 'ReconcilledOrders',
+        key: '8',
+        link: 'complaints',
+        text: 'Reconcilled Orders',
+    },
+];
+
+export const DASHBOARD_MENU_STRING_FOUR = [
+    {
+        iconName: 'Merchant',
+        key: '9',
+        link: '',
+        text: 'Merchant Profile',
+    }
+];
